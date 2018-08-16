633356cddd8ab9aa9b9ad0abc5d9031ad305b57f
Lang <hoanglang@gmail.com>
2018-08-17 00:08:44 +0200

    Closure

--- js/gestalt.js
+++ js/gestalt.js
@@ -12,22 +12,29 @@ class Gestalt {
             0, 0, 0, 0,
             0, 0, 0, 0,
         ];
+        // TODO
+        this.bigRadius = Math.floor(Math.min(width, height) * .3);
 
-        this.drawGraph();
+        this.initGraphs();
     }
 
 
     /**
-     * Création du SVG.
+     * Création des SVG.
      */
-    drawGraph() {
-        const svg = d3.select('#graph')
-            .append('svg')
-            .attr('height', this.height)
-            .attr('width', this.width);
+    initGraphs() {
+        function createSvg(container, classname) {
+            d3.select(container + ' .graph')
+                .append('svg')
+                .attr('height', this.height)
+                .attr('width', this.width)
+                .append('g')
+                .classed(classname, true);
+        }
 
-        svg.append('g').classed('circles', true);
-
+        createSvg.call(this, '#container1', 'circles');
+        createSvg.call(this, '#container2', 'shapes');
+        this.closureShapes();
     }
 
     /**
@@ -35,8 +42,8 @@ class Gestalt {
      */
     init() {
         // Join
-        const circles = d3.select(".circles")
-            .selectAll(".circle")
+        const circles = d3.select('#container1 .circles')
+            .selectAll('.circle')
             .data(this.data);
         const t = d3.transition().duration(2000);
 
@@ -46,7 +53,7 @@ class Gestalt {
             .attr('cx', (d, i) => this.offsetX + (i % 4) * this.interval)
             .attr('cy', (d, i) => this.offsetY + Math.floor(i / 4) * this.interval)
             .attr('fill', 'rgb(31, 119, 180)')
-            .attr('class', "circle");
+            .attr('class', 'circle');
 
         // Enter
         circles.enter()
@@ -54,7 +61,7 @@ class Gestalt {
             .attr('r', 0)
             .attr('cx', (d, i) => this.offsetX + (i % 4) * this.interval)
             .attr('cy', (d, i) => this.offsetY + Math.floor(i / 4) * this.interval)
-            .attr('class', "circle")
+            .attr('class', 'circle')
             .attr('fill', 'rgb(31, 119, 180)')
             .transition(t)
             .attr('r', this.radius);
@@ -68,8 +75,8 @@ class Gestalt {
         const divide = this.interval;
         const t = d3.transition().duration(2000);
 
-        const circles = d3.select(".circles")
-            .selectAll(".circle")
+        const circles = d3.select('#container1 .circles')
+            .selectAll('.circle')
             .transition(t)
             .attr('cx', (d, i) => {
                 let modulo = (i % 4);
@@ -80,27 +87,152 @@ class Gestalt {
                 return result;
             })
             .attr('cy', (d, i) => {
-                let result =  this.offsetY + Math.floor(i / 4) * newInterval;
+                let result = this.offsetY + Math.floor(i / 4) * newInterval;
                 if (i > 7) {
                     result += divide;
                 }
                 return result;
 
             })
-            .attr('class', "circle");
+            .attr('class', 'circle');
     }
 
+    /**
+     * Similarity.
+     */
     similarity() {
         this.init();
-        const t = d3.transition().duration(2000);
+        const t = d3.transition().duration(1000);
 
-        const circles = d3.select(".circles")
-            .selectAll(".circle")
+        const circles = d3.select('#container1 .circles')
+            .selectAll('.circle')
             .transition(t).delay(2000)
             .attr('fill', (d, i) => (i > 7) ? 'red' : 'rgb(31, 119, 180)');
+
+        // TODO : remplacer les circle par des rect ?
     }
 
 
+    /**
+     * Initialisation des formes pour la partie 2.
+     */
+    closureShapes() {
+        const x0 = Math.floor(this.width/2);
+        const y0 = Math.floor(this.height/2);
+        const c = 80;
+        const square =
+            `M ${x0 - c}, ${y0 - c/2}
+             v ${-c /2}
+             h ${c * 2}         
+             v ${c * 2}         
+             h ${-c * 2} 
+             Z       
+        `;
+
+        const triangle =
+            `M ${x0 + c/2}, ${y0 - c * 2}
+             L ${x0 + c * 7/4}, ${y0}
+             h ${-c * 2.5}
+             Z
+            `;
+
+        const svg = d3.select('#container2 .graph .shapes');
+        svg.append('path')
+            .attr('id', 'closure-triangle')
+            .attr('stroke','black')
+            .attr('fill','red')
+            .attr('stroke-width', 4)
+            .attr('fill-opacity', 1)
+            .attr('stroke-dasharray', '')
+            .attr('d', triangle);
+
+        svg.append('circle')
+            .attr('id', 'closure-circle')
+            .attr('cx', x0 - c)
+            .attr('cy', y0 - c)
+            .attr('r', c)
+            .attr('stroke','black')
+            .attr('fill','yellow')
+            .attr('stroke-width', 4)
+            .attr('fill-opacity', 1)
+            .attr('stroke-dasharray', '');
+
+
+        svg.append('path')
+            .attr('id', 'closure-square')
+            .attr('stroke','black')
+            .attr('fill','green')
+            .attr('stroke-width', 4)
+            .attr('fill-opacity', 1)
+            .attr('stroke-dasharray', '')
+            .attr('d', square);
+
+    }
+
+    /**
+     * Closure part 1.
+     */
+    closure1() {
+        const c = 80;
+        const t1 = d3.transition().duration(3000);
+        const t2 = d3.transition().duration(2000);
+
+        const svg = d3.select('#container2 .graph .shapes');
+        svg.select('#closure-triangle')
+            .transition(t1)
+            .attr('fill-opacity', 1)
+            .attr('transform', 'translate(0, 0)')
+            .transition(t2)
+            .attr('stroke-dasharray', '')
+            ;
+
+        svg.select('#closure-circle')
+            .transition(t1)
+            .attr('fill-opacity', 1)
+            .attr('transform', 'translate(0, 0)')
+            .transition(t2)
+            .attr('stroke-dasharray', '');
+
+        svg.select('#closure-square')
+            .transition(t1)
+            .attr('fill-opacity', 1)
+            .attr('transform', 'translate(0, 0)')
+            .transition(t2)
+            .attr('stroke-dasharray', '');
+
+    }
+
+    /**
+     * Closure part 2.
+     */
+    closure2() {
+        const c = 80;
+        const t1 = d3.transition().duration(3000);
+        const t2 = d3.transition().duration(2000);
+
+        const svg = d3.select('#container2 .graph .shapes');
+        svg.select('#closure-triangle')
+            .transition(t1)
+            .attr('fill-opacity', 0)
+            .attr('transform', 'translate(50, -50)')
+            .transition(t2)
+            .attr('stroke-dasharray', '40 100 100 100 100 100');
+
+        svg.select('#closure-circle')
+            .transition(t1)
+            .attr('fill-opacity', 0)
+            .attr('transform', 'translate(-50, -50)')
+            .transition(t2)
+            .attr('stroke-dasharray', c);
+
+        svg.select('#closure-square')
+            .transition(t1)
+            .attr('fill-opacity', 0)
+            .attr('transform', 'translate(0, 50)')
+            .transition(t2)
+            .attr('stroke-dasharray', c);
+
+    }
 
 
     display(index) {
